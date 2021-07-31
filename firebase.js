f05f533eeb4c0be2d5930c7aec0c7ef039cf114e
Ben Carter <benjcarter06@gmail.com>
2021-07-31 14:43:55 -0300

    day 3

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAv_cSeD7aWcIgZ9K2HKZOJ6pPEV2Id-4Y",
+    authDomain: "amzn-2-8f899.firebaseapp.com",
+    projectId: "amzn-2-8f899",
+    storageBucket: "amzn-2-8f899.appspot.com",
+    messagingSenderId: "896127229435",
+    appId: "1:896127229435:web:1c706a624eff4fed77ff5e"
+};
+
+firebase.initializeApp(firebaseConfig);
